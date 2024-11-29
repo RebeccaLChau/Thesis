3d635bd972b1a51271e609c32713e8a49cc99fa3
RebeccaLChau <rebeccachau@live.co.uk>
2024-11-29 10:50:53 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -24,15 +24,15 @@ dataset = datasets.ImageFolder(root=path_dataset, transform=transformation)
 trainingset, testset = torch.utils.data.random_split(dataset, [int(len(dataset)*0.99), len(dataset)-int(len(dataset)*0.99)])
 
 test_loader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False, drop_last=True, pin_memory=False)
-
+train_loader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False, drop_last=True, pin_memory=False)
 # In[3]: Train the model
 
-latent_dimension = 128
+latent_dimension = 64
 beta = 1
-num_epochs = 10
+num_epochs = 10 
 
-vae = Model.VAE(latent_dimension=latent_dimension, beta=beta)
-vae = Train.train_vae(vae, trainingset, num_epochs, beta, k_folds=10, latent_dimension=latent_dimension)
+vae = Model.VAE(latent_dimension=latent_dimension, beta=beta).to(device)
+vae = Train.train_vae(vae, train_loader, num_epochs, beta)
 
 with torch.no_grad():
     for x, _ in test_loader:
@@ -45,33 +45,33 @@ fname = 'vae_model'  # You can customize this based on parameters like `latent_d
 torch.save(vae.state_dict(), save_path + 'state_' + fname + '.st')
 torch.save(vae, save_path + fname + '.pth')
 
-_, mu, lv = vae.forward(x)
-z = vae.reparameterize(mu,lv)
+#_, mu, lv = vae.forward(x)
+#z = vae.reparameterize(mu,lv)
 
-z2 = torch.clone(z)
-beg=-1.8
-end=1.8
-stp=0.4
-znew = z2[0]
+#z2 = torch.clone(z)
+#beg=-1.8
+#end=1.8
+#stp=0.4
+#znew = z2[0]
 
-for i in np.arange(beg,end,stp):
-    for j in np.arange(beg,end,stp):
-        zinit = torch.clone(z2[0])
-        zinit[:int(latent_dimension/2)] += i
-        zinit[int(latent_dimension/2):int(latent_dimension)] += j
-        znew = torch.cat((znew, zinit), 0)
+#for i in np.arange(beg,end,stp):
+   # for j in np.arange(beg,end,stp):
+ #       zinit = torch.clone(z2[0])
+  #      zinit[:int(latent_dimension/2)] += i
+    #    zinit[int(latent_dimension/2):int(latent_dimension)] += j
+  #      znew = torch.cat((znew, zinit), 0)
 
-znew = znew.reshape((-1,latent_dimension))
-tnew = vae.decode(znew)
-lx = len(np.arange(beg,end,stp))
-ly = len(np.arange(beg,end,stp))
-fig, axes = plt.subplots(nrows=ly, ncols=lx, figsize = (16,16), sharex=True, sharey=True)
-plt.subplots_adjust(wspace=0, hspace=-0.5)
-k=0
+#znew = znew.reshape((-1,latent_dimension))
+#tnew = vae.decode(znew)
+#lx = len(np.arange(beg,end,stp))
+#ly = len(np.arange(beg,end,stp))
+#fig, axes = plt.subplots(nrows=ly, ncols=lx, figsize = (16,16), sharex=True, sharey=True)
+#plt.subplots_adjust(wspace=0, hspace=-0.5)
+#k=0
 
-for i in range(lx):
-    for j in range(ly):
-        axes[i][j].imshow(tnew[k].permute((1,2,0)).detach().cpu().numpy(), interpolation='lanczos')
-        k+=1
-plt.show()
+#for i in range(lx):
+#    for j in range(ly):
+#        axes[i][j].imshow(tnew[k].permute((1,2,0)).detach().cpu().numpy(), interpolation='lanczos')
+#        k+=1
+#plt.show()
 
